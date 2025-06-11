7981f043ae1284692b8bf0f2c9668cf01a7d472c
Shnabbydoodle <meng.lawrence@gmail.com>
2025-06-11 16:14:42 -0400

    yarg.

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Header from './components/header'
 import Home from './pages/home'
+import About from './pages/about'
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <div className="content">
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
         </Routes>
       </div>
     </Router>
