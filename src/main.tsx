be8cebf4981227a50e629421a8d851456817fc68
Shnabbydoodle <meng.lawrence@gmail.com>
2025-06-11 21:51:42 -0400

    jeez you really can't have any warnings in here either. Commented out two imports that were not used.

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
-import { StrictMode } from 'react'
+//import { StrictMode } from 'react'
 import React from 'react'
-import { createRoot } from 'react-dom/client'
+//import { createRoot } from 'react-dom/client'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
