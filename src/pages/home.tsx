d1c62c7340e7c7b2c3798f79e5aa3269d271bc92
Shnabbydoodle <meng.lawrence@gmail.com>
2025-06-11 23:51:04 -0400

    todolist

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -5,6 +5,14 @@ export default function Home() {
       <h1>Positive Bonus</h1>
       <p>This website teaches Guilty Gear Strive at all levels of play.</p>
       <h2>WIP!!</h2>
+      <p>To-Do:</p>
+      <ul>
+        <li>more bg images</li>
+        <li>in document images</li>
+        <li>text formatting and highlighting</li>
+        <li>advanced mode</li>
+      </ul>
+      
     </div>
     </div>
   )
