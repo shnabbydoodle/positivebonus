9d5c4a5d738bf4381300cd04d18a0a620c4fda39
Shnabbydoodle <meng.lawrence@gmail.com>
2025-06-11 22:40:40 -0400

    uhhhhhh positive bonus popout triangle thing

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,12 +1,16 @@
 import { Link } from 'react-router-dom'
 import Outrage from '../assets/outrage.png'
+import './header.css'
 
 export default function Header() {
   return (
     <header className="header">
+        <Link to="/" className="corner-link" aria-label="Home">
+         <div className="corner-text">POSITIVE</div>
+        </Link>
         <div className="headerImageContainer"><img src={Outrage} alt="The Outrage MK2" className="headerImage" /></div>
         <nav>
-            <Link to="/" style={{ marginRight: '1rem' }}>Home</Link>
+            <Link to="/" className="mobileHomeLink" style={{ marginRight: '1rem' }}>Home</Link>
             <Link to="/about">About</Link>
         </nav>
     </header>
