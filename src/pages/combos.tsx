135ddfdfc1b342d0fb622919a1164baa80e423fb
Essull <ess6446@rit.edu>
2025-06-12 01:17:52 -0400

    Info on combo notation

--- src/pages/combos.tsx
+++ src/pages/combos.tsx
@@ -23,5 +23,17 @@ export default function Combos() { return ( <div className='mainWrapper'>
       <li>Metered vs meterless combos: Do you have 50 tension ready? If so, you can use Red Roman Cancels to extend combos past usual limits, or cancel a normal into a super to cash out more damage!</li>
       <li>Midscreen vs corner: Having the opponent against the wall in the corner means they can't get flung away from you during your combo. This allows every character to do optimized corner combos which deal more damage, and usually result in a wall break.</li>
     </ul>
+    <h3>How to read combo and write combo notation</h3>
+    <ul>
+      <li>Combo Notation may look something like AACHcs = cs &gt; DC623s~s~66 &gt; 2k &gt; 6h &gt; 236h~66 kara 236236&#91;p&#93;~&#93;p&#91;</li>
+      <li> <strong>X</strong> is often used as a placeholder for a button, usually meaning anybutton works there</li>
+      <li> <strong>&gt;</strong> means that that6 move cancels into the next move</li>
+      <li> <strong>=</strong> means to link one button into the other, for more info on how this works see frame data</li>
+      <li> <strong>kara</strong> this one is a more advanced technique and has its own section, see below</li>
+      <li> <strong>CH</strong> stands for Counter Hit. This is when you see sparks aroudn the enemy when you hit them; and/or when the game slows down</li>
+      <li> <strong>dl.</strong> stands for delay, meanign to delay the pressing of the next button, usually so that the move will not whiff</li>
+      <li> <strong>DC</strong> This stands for Dash Cancel, this has more uses with kara cancels, once again see below for more info</li>
+      <li> <strong>&#91;X&#93;</strong> and <strong>&#93;X&#91;</strong> These refer to holding down a button and releasing a buttons</li>
+    </ul>
   </div>
 </div> ) }
