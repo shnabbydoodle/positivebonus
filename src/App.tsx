1f4e46e4f2f95c5537adf2e3e3464df1da40dfab
Shnabbydoodle <meng.lawrence@gmail.com>
2025-06-12 13:49:21 -0400

    optimized code

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
-/**import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'**/
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useLocation, Routes, Route } from 'react-router-dom'
+
 import HomePage from './pages/home'
 import AboutPage from './pages/about'
 import StartPage from './pages/start'
@@ -12,63 +10,72 @@ import AdvantagePage from './pages/advantage'
 import NeutralPage from './pages/neutral'
 import OffensePage from './pages/offense'
 import CombosPage from './pages/combos'
+
 import bg0 from './assets/bg.jpg'
 import bg1 from './assets/bg1.jpg'
 import bg2 from './assets/bg2.jpg'
 import bg3 from './assets/bg3.jpg'
 import bg4 from './assets/bg4.jpg'
-import sf6 from './assets/sf6.jpg'
+//import sf6 from './assets/sf6.jpg'
 
 import Header from './components/header'
 
-
 function App() {
-  const last =localStorage.getItem("bg") //grab the last background image number
   const location = useLocation()
-  const arr = [bg0,bg1,bg2,bg3,bg4,sf6] //all the images
-  let bg = ""
-  let index= 0
-  while(true){ //loops until the last background image is different than the randonly selected one
-    index = Math.floor((Math.random()*10)%5) //random number between 0-4
-    if(index.toString() != last){ // if the numbers dont match we're good to go, if they do match we loop
-      bg= arr[index]
-      break
-    }
-  }
+  const arr = [bg0, bg1, bg2, bg3, bg4]
 
-  window.localStorage.setItem("bg",index.toString()) //set background image
+  const [bg, setBg] = useState<string>('')
 
-  
-  
-  
+useEffect(() => {
+  const last = localStorage.getItem("bg");
+
+  //filter indices
+  const availableIndices = arr
+    .map((_, i) => i.toString())
+    .filter(i => i !== last);
+
+  //choose new index
+  const index = parseInt(
+    availableIndices[Math.floor(Math.random() * availableIndices.length)],
+    10
+  );
+
+  const selectedBg = arr[index];
+  localStorage.setItem("bg", index.toString());
+
+  // preload image
+  const img = new Image();
+  img.src = selectedBg;
+
+  img.onload = () => {
+    setBg(selectedBg); // only set when loaded
+  };
+
+}, [location.pathname]);
+ // only rerun when the route changes
+
   useEffect(() => {
-    // Add background class only on home page
-    //except im gonna put it on everything for now
-    /*if (location.pathname === '/') {
+    if (bg) {
+      document.body.style.backgroundImage = `url(${bg})`
       document.body.classList.add('homepage-bg')
-    } else {
-      document.body.classList.remove('homepage-bg')
-    }    */
-   document.body.style.backgroundImage ='url('+bg+')' //adds styling to the body for abckground image
-   document.body.classList.add('homepage-bg') //this adds it to everything
-  }, [location.pathname])
+    }
+  }, [bg]) // only run this when the background is set
 
   return (
     <>
-    <div></div>
       <Header />
-      <div className='content'>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/about" element={<AboutPage />} />
-        <Route path="/start" element={<StartPage />} />
-        <Route path="/vocabulary" element={<VocabPage />} />
-        <Route path="/defense" element={<DefensePage />} />
-        <Route path="/advantage" element={<AdvantagePage />} />
-        <Route path="/neutral" element={<NeutralPage />} />
-        <Route path="/offense" element={<OffensePage />} />
-        <Route path="/combos" element={<CombosPage />} />
-      </Routes>
+      <div className="content">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/start" element={<StartPage />} />
+          <Route path="/vocabulary" element={<VocabPage />} />
+          <Route path="/defense" element={<DefensePage />} />
+          <Route path="/advantage" element={<AdvantagePage />} />
+          <Route path="/neutral" element={<NeutralPage />} />
+          <Route path="/offense" element={<OffensePage />} />
+          <Route path="/combos" element={<CombosPage />} />
+        </Routes>
       </div>
     </>
   )
