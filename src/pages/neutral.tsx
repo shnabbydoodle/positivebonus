c7e923b82c5923a86561579ff241f4afb93185a1
Shnabbydoodle <meng.lawrence@gmail.com>
2025-06-11 23:41:11 -0400

    edited youtube link

--- src/pages/neutral.tsx
+++ src/pages/neutral.tsx
@@ -14,7 +14,7 @@ export default function Neutral() { return ( <div className='mainWrapper'>
       <li>Passively waiting for somebody to swing their giant button will beat it, since their preemptive strike often assumes you will close the gap first. Therefore, it will whiff and you can react to it missing you and run up and counterattack.</li>
       <li>At a very low level, people will be flailing in neutral (preemptively swinging). Therefore, passive play beats a lot of low level players since you can just wait for them to do something stupid and then counterattack.</li>
       <li>
-        <strong> WATCH THIS VIDEO!</strong>
+        <strong> <a href="https://www.youtube.com/watch?v=tjmnNbG0HAw">WATCH THIS VIDEO!!</a></strong>
       </li>
       <li>Neutral is a relatively complicated topic to distill into simple terms and the rapidly changing nature of every neutral interaction doesn't make it any easier to learn. However, do try to think about the rock-paper-scissors when you are playing since it will help you comprehend what you are looking at.</li>
     </ul>
