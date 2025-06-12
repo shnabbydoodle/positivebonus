6569d86c7b55eedd81b29f888f7ab8715ed8a44e
Shnabbydoodle <meng.lawrence@gmail.com>
2025-06-11 21:59:37 -0400

    favicon and title

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 //import { StrictMode } from 'react'
 import React from 'react'
 //import { createRoot } from 'react-dom/client'
+//NOTE: NOT COMMENTING OUT UNUSED DECLARATIONS CAUSES CLOUDFLARE TO EAT SHIT AND DIE!
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
